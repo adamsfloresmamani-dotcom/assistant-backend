dc256f665347e682e77db04535f6e99f3f336b8c
adamsfloresmamani-dotcom <adamsfloresmamani@gmail.com>
2025-12-09 22:56:10 -0500

    Update server.py

--- server.py
+++ server.py
@@ -1,25 +1,20 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, send_from_directory
 from stt import transcribe
 from tts import speak
 from planner import plan_day
 import base64
 import os
-from flask_cors import CORS
 
-app = Flask(__name__)
-CORS(app)
+app = Flask(__name__, static_url_path='', static_folder='.')
 
 @app.post("/server")
 def server():
-
     if "audio" not in request.files:
         return jsonify({"error": "No se envió audio"}), 400
 
-    file = request.files["audio"]
+    audio = request.files["audio"]
 
-    audio_bytes = file.read()
-
-    text = transcribe(audio_bytes)
+    text = transcribe(audio)
     plan = plan_day(text)
     voice_mp3 = speak(plan)
 
@@ -28,6 +23,9 @@ def server():
         "voice": base64.b64encode(voice_mp3).decode()
     })
 
+@app.get("/")
+def index():
+    return send_from_directory(".", "index.html")
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
